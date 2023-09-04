4ac650d4df7dcb0bb3370de9e2937c35bd96eeec
ALBERT O MATARA <137197284+Mbappe-254@users.noreply.github.com>
2023-09-04 15:09:19 +0000

    Update 5 files

---
+++ src/features/icecreams/CreamDetail.js
@@ -0,0 +1,16 @@
+import { Card, CardBody, CardImg, CardText, Col } from "reactstrap";
+
+const CreamDetail = ({icecream}) => {
+    const { image, name, description } = icecream ;
+    return (
+        <Col md='5' className='m-1'>
+            <Card>
+                <CardImg top src={image} alt={name} />
+                <CardBody>
+                    <CardText>{description}</CardText>
+                </CardBody>
+            </Card>
+        </Col>
+    )
+};
+export default CreamDetail;
---
+++ src/features/icecreams/CreamList.js
@@ -0,0 +1,30 @@
+import { useSelector } from 'react-redux';
+import { Col, Row } from 'reactstrap';
+import CreamCard from "./CampsiteCard";
+import { selectAllIcecreams } from './CreamSlice';
+
+const CreamList =() => {
+  const icecreams = useSelector(selectAllIcecreams);
+  console.log('icecreams:', icecreams);
+  return (
+
+    <Row className='ms-auto'>
+      {icecreams.map((icecream) => {
+
+        return (
+          <Col
+            md='5'
+            className='m-4'
+            key={icecream.id}
+          >
+            <CreamCard icecream={icecream} />
+          </Col>
+        );
+
+      })}
+
+    </Row>
+
+  );
+}
+export default CreamList;
